065b9ab9f32e6c5d641e69aafe30af8ed7df5f61
josephtheceo <159775398+josephkplp@users.noreply.github.com>
2024-10-30 08:24:04 +0300

    Update forms.py
    created the loginForm

---
+++ benedict/benedict_school/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        max_length=50,
+        widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'})
+    )
+
